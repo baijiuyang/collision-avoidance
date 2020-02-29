34aed1e6f71ca9f47d6757802f11a1d5eb1ca47c
Jiuyang Bai <jiuyang_bai@brown.edu>
2020-02-28 19:01:19 -0500

    Added collision_trajectory to helper.py

--- simulations/helper.py
+++ simulations/helper.py
@@ -1,8 +1,12 @@
-# This module contains all the helper functions for my dissertation research on 
-# pedestrian avoidance
+# This module contains all the helper functions for the dissertation research of Jiuyang Bai
+# on the control law of pedestrian collision avoidance please contact baijiuyang@hotmail.com
+# for support. Github: https://github.com/baijiuyang/collision-avoidance.git
+
 import numpy as np
 from numpy.linalg import norm
+from numpy import sqrt
 import math
+from matplotlib import animation, pyplot as plt
 
 
 def rate_of_expansion(p0, p1, v0, v1, w, relative=False):
@@ -133,8 +137,75 @@ def minimum_separation(p0, p1, v0, v1):
     return dca, ttca
 
     
+def collision_trajectory(bearing, side, spd1=2.0, w=1.5, r=10, Hz=100, animate=False, interval=None):
+    '''
+    This function produces near-collision trajectories of circular agents 0 and 1 with 
+    a defined bearing angle. The speed of agent 1 is constant. The speed of agent 0 will 
+    vary to guarantee near-collision. 
     
+    Args:
+        bearing ((-90,90), float): The initial bearing angle between A and B in degree. Bearing 
+            angle is defined as the angle between the line of sight and heading direction.        
+        side (char): Agent 0 pass in front ('f'), or from behind ('b') agent 1. 
+        spd1 (float): The speed of agent 1 in meter/second.
+        w (float): The diameter of both agents in meters.
+        r (float): The distance from agent 1 to origin of the coordinate system. 
+        Hz (int): Number of Hz for the simulation.
+        animate (bool): Whether animate the trajectories.
+        interval (int): Delay between frames in milliseconds.
+        
+    Return:
+        traj0, traj1 (2-d np array): Near-collision trajectories of agent 1 and agent 0 in 
+            meters.
+        v0[1] (float): The speed of agent 0 to achieve near-collision with agent 1.
+    '''
+    alpha = (180 - 2 * bearing) * 2 * math.pi / 360 # Central angle of line of sight
+    p1 = np.array([-r * np.sin(alpha), -r * np.cos(alpha)])
+    p0 = np.array([0, -r])
+    p01 = p1 - p0
+    v1 = np.array([spd1 * np.sin(alpha), spd1 * np.cos(alpha)])
+    a = r ** 2 * np.sin(alpha) ** 2 - w ** 2
+    b = -2 * spd1 * (np.cos(alpha) * (norm(p01) ** 2 - w ** 2) + r ** 2 * (1 - np.cos(alpha)) ** 2)
+    c = spd1 ** 2 * (norm(p01) ** 2 - w ** 2 - r ** 2 * (1 - np.cos(alpha)) ** 2)
+    d = (b ** 2) - (4 * a * c)
+
+    # find two solutions
+    sol1 = (-b - sqrt(d)) / (2 * a)
+    sol2 = (-b + sqrt(d)) / (2 * a)
     
+    if side == 'f':
+        v0 = np.array([0, max(sol1, sol2)])
+    elif side == 'b': 
+        v0 = np.array([0, min(sol1, sol2)])
+    else:
+        raise Exception('Argument side should be either \'f\' or \'b\'')
+    t = 2 * r / spd1
+    n = int(t * Hz)
+    traj0 = np.cumsum(np.tile(v0 / Hz, (n, 1)), axis=0) + np.expand_dims(p0, axis=0)
+    traj1 = np.cumsum(np.tile(v1 / Hz, (n, 1)), axis=0) + np.expand_dims(p1, axis=0)
+    
+    if animate:
+        if not interval: interval = 1000 / Hz
+        fig = plt.figure(figsize=(7, 7))
+        ax = plt.axes(xlim=(-12, 12), ylim=(-12, 12))
+        angles = np.linspace(0, 2 * math.pi, num=12)
+        circle = np.stack((w / 2 * np.cos(angles), w / 2 * np.sin(angles)), axis=-1)
+        agent0, = ax.plot(traj0[0, 0] + circle[:, 0], traj0[0, 1] + circle[:, 1], 'b')
+        agent1, = ax.plot(traj1[0, 0] + circle[:, 0], traj1[0, 1] + circle[:, 1], 'r')
+        def animate_fast(i):
+            '''
+                Fast animation function update without clear. Good for
+                watching in real time, but will leave trace if saved.
+            '''
+            # ms is the short for markersize
+            agent0.set_data(traj0[i, 0] + circle[:, 0], traj0[i, 1] + circle[:, 1])
+            agent1.set_data(traj1[i, 0] + circle[:, 0], traj1[i, 1] + circle[:, 1])
+
+            return agent0, agent1
+        # call the animator.  blit=True means only re-draw the parts that have changed.
+        anim = animation.FuncAnimation(fig, animate_fast, frames=len(traj0), interval=interval, blit=True)
+    
+    return traj0, traj1, v0[1]
     
     
     
